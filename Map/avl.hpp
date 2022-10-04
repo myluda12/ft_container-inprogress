95eb908e129d8e6af649d75c4592496b1978df9c
Ayoub Ajrhourh <ayajrhou@e2r12p4.1337.ma>
2022-10-04 08:28:58 +0100

    yees

--- Map/avl.hpp
+++ Map/avl.hpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include "utils.hpp"
-namespace ft {
+namespace ft 
+{
 
 
 
@@ -87,161 +88,109 @@ class avl {
         }
     }
     
-    // Function to update the height of
-    // a node according to its children's
-    // node's heights
-    void Updateheight(
-        node_type* root)
+    void Updateheight(node_type* root)
     {
-        if (root != NULL) {
-    
-            // Store the height of the
-            // current node
+        if (root != NULL) 
+        {
+
             int val = 1;
-    
-            // Store the height of the left
-            // and right subtree
             if (root->left != NULL)
                 val = root->left->height + 1;
     
             if (root->right != NULL)
                 val = max(val, root->right->height + 1);
-    
-            // Update the height of the
-            // current node
             root->height = val;
         }
     }
     
-    // Function to handle Left Left Case
-    node_type* LLR(
-        node_type* root)
+    node_type* Left_Left_rotate(node_type* root)
     {
-        // Create a reference to the
-        // left child
+
         node_type* tmpnode = root->left;
     
-        // Update the left child of the
-        // root to the right child of the
-        // current left child of the root
         root->left = tmpnode->right;
     
-        // Update parent pointer of left
-        // child of the root node
         if (tmpnode->right != NULL)
             tmpnode->right->par = root;
     
-        // Update the right child of
-        // tmpnode to root
+
         tmpnode->right = root;
     
-        // Update parent pointer of tmpnode
         tmpnode->par = root->par;
     
-        // Update the parent pointer of root
         root->par = tmpnode;
     
-        // Update tmpnode as the left or
-        // the right child of its parent
-        // pointer according to its key value
-        if (tmpnode->par != NULL
-            && _comp(root->pair->first, tmpnode->par->pair->first)) {
+
+        if (tmpnode->par != NULL && _comp(root->pair->first, tmpnode->par->pair->first))
             tmpnode->par->left = tmpnode;
-        }
         else {
             if (tmpnode->par != NULL)
                 tmpnode->par->right = tmpnode;
         }
     
-        // Make tmpnode as the new root
         root = tmpnode;
     
-        // Update the heights
         Updateheight(root->left);
         Updateheight(root->right);
         Updateheight(root);
         Updateheight(root->par);
     
-        // Return the root node
         return root;
     }
     
-    // Function to handle Right Right Case
-    node_type* RRR(
-        node_type* root)
+    node_type* Right_Right_rotate (node_type* root)
     {
-        // Create a reference to the
-        // right child
+
         node_type* tmpnode = root->right;
     
-        // Update the right child of the
-        // root as the left child of the
-        // current right child of the root
         root->right = tmpnode->left;
     
-        // Update parent pointer of the
-        // right child of the root node
         if (tmpnode->left != NULL)
             tmpnode->left->par = root;
     
-        // Update the left child of the
-        // tmpnode to root
+
         tmpnode->left = root;
     
-        // Update parent pointer of tmpnode
+
         tmpnode->par = root->par;
     
-        // Update the parent pointer of root
+
         root->par = tmpnode;
     
-        // Update tmpnode as the left or
-        // the right child of its parent
-        // pointer according to its key value
-        if (tmpnode->par != NULL
-            && _comp(root->pair->first, tmpnode->par->pair->first)) {
+
+        if (tmpnode->par != NULL && _comp(root->pair->first, tmpnode->par->pair->first)) 
             tmpnode->par->left = tmpnode;
-        }
         else {
             if (tmpnode->par != NULL)
                 tmpnode->par->right = tmpnode;
         }
     
-        // Make tmpnode as the new root
         root = tmpnode;
     
-        // Update the heights
         Updateheight(root->left);
         Updateheight(root->right);
         Updateheight(root);
         Updateheight(root->par);
     
-        // Return the root node
         return root;
     }
     
-    // Function to handle Left Right Case
-    node_type* LRR(
-        node_type* root)
+    node_type* Left_Right_Right(node_type* root)
     {
-        root->left = RRR(root->left);
-        return LLR(root);
+        root->left = Right_Right_rotate(root->left);
+        return Left_Left_rotate(root);
     }
     
-    // Function to handle right left case
-    node_type* RLR(
-        node_type* root)
+    node_type* Right_Left_rotate(node_type* root)
     {
-        root->right = LLR(root->right);
-        return RRR(root);
+        root->right = Left_Left_rotate(root->right);
+        return Right_Right_rotate(root);
     }
     
-    // Function to balance the tree after
-    // deletion of a node
-    node_type* Balance(
-        node_type* root)
+
+    node_type* Balance(node_type* root)
     {
-        // Store the current height of
-        // the left and right subtree
+
         int firstheight = 0;
         int secondheight = 0;
     
@@ -251,14 +200,11 @@ class avl {
         if (root->right != NULL)
             secondheight = root->right->height;
     
-        // If current node is not balanced
-        if (abs(firstheight - secondheight) == 2) {
-            if (firstheight < secondheight) {
+        if (abs(firstheight - secondheight) == 2) 
+        {
+            if (firstheight < secondheight) 
+            {
     
-                // Store the height of the
-                // left and right subtree
-                // of the current node's
-                // right subtree
                 int rightheight1 = 0;
                 int rightheight2 = 0;
                 if (root->right->right != NULL)
@@ -269,21 +215,14 @@ class avl {
     
                 if (rightheight1 > rightheight2) {
     
-                    // Right Left Case
-                    root = RLR(root);
+                    root = Right_Left_rotate(root);
                 }
-                else {
-    
-                    // Right Right Case
-                    root = RRR(root);
-                }
+                else 
+                    root = Right_Right_rotate(root);
             }
-            else {
-    
-                // Store the height of the
-                // left and right subtree
-                // of the current node's
-                // left subtree
+            else 
+            {
+
                 int leftheight1 = 0;
                 int leftheight2 = 0;
                 if (root->left->right != NULL)
@@ -292,20 +231,12 @@ class avl {
                 if (root->left->left != NULL)
                     leftheight1 = root->left->left->height;
     
-                if (leftheight1 > leftheight2) {
-    
-                    // Left Left Case
-                    root = LLR(root);
-                }
-                else {
-    
-                    // Left Right Case
-                    root = LRR(root);
-                }
+                if (leftheight1 > leftheight2) 
+                    root = Left_Left_rotate(root);
+                else
+                    root = Left_Right_Right(root);
             }
         }
-    
-        // Return the root node
         return root;
     }
     
@@ -328,10 +259,8 @@ class avl {
     node_type* Insert(node_type* root, node_type* parent, pair _pair)
     {
        // std::cout << "Pair" << _pair.first << ">" << _pair.second << std::endl;
-        if (root == NULL) {
-    
-            // Create and assign values
-            // to a new node
+        if (root == NULL) 
+        {
             root = _NodeAll.allocate(1);
             // if (root == NULL)
             //     std::cout << "Error in memory" << std::endl;
@@ -346,15 +275,10 @@ class avl {
                 return root;
         }
     
-        else if (!_comp(root->pair->first, _pair.first)) {
-    
-            // Recur to the left subtree
-            // to insert the node
-            root->left = Insert(root->left,
-                                root, _pair);
-    
-            // Store the heights of the
-            // left and right subtree
+        else if (!_comp(root->pair->first, _pair.first)) 
+        {
+            root->left = Insert(root->left,root, _pair);
+
             int firstheight = 0;
             int secondheight = 0;
     
@@ -364,34 +288,20 @@ class avl {
             if (root->right != NULL)
                 secondheight = root->right->height;
     
-            // Balance the tree if the
-            // current node is not balanced
             if (abs(firstheight - secondheight) == 2) 
             {
     
-                if (root->left != NULL
-                    && _comp(_pair.first, root->left->pair->first)) {
-    
-                    // Left Left Case
-                    root = LLR(root);
-                }
-                else {
-    
-                    // Left Right Case
-                    root = LRR(root);
-                }
+                if (root->left != NULL && _comp(_pair.first, root->left->pair->first)) 
+                    root = Left_Left_rotate(root);
+                else
+                    root = Left_Right_Right(root);
             }
         }
     
-        else if (_comp(root->pair->first, _pair.first)) {
-    
-            // Recur to the right subtree
-            // to insert the node
-            root->right = Insert(root->right,
-                                root, _pair);
-    
-            // Store the heights of the left
-            // and right subtree
+        else if (_comp(root->pair->first, _pair.first))
+        {
+            root->right = Insert(root->right,root, _pair);
+
             int firstheight = 0;
             int secondheight = 0;
     
@@ -400,49 +310,29 @@ class avl {
     
             if (root->right != NULL)
                 secondheight = root->right->height;
-    
-            // Balance the tree if the
-            // current node is not balanced
-            if (abs(firstheight - secondheight) == 2) {
-                if (root->right != NULL
-                    && _comp(_pair.first, root->right->pair->first)) {
-    
-                    // Right Left Case
-                    root = RLR(root);
-                }
-                else {
-    
-                    // Right Right Case
-                    root = RRR(root);
-                }
+
+            if (abs(firstheight - secondheight) == 2) 
+            {
+                if (root->right != NULL && _comp(_pair.first, root->right->pair->first)) 
+                    root = Right_Left_rotate(root);
+                else
+                    root = Right_Right_rotate(root);
             }
         }
-    
-        // Case when given key is
-        // already in tree
-        else {
+        else 
             return root;
-        }
     
-        // Update the height of the
-        // root node
         Updateheight(root);
-    
-        // Return the root node
+
         return root;
     }
-    
-    // Function to delete a node from
-    // the AVL tree
+
     node_type* Delete(node_type* root,pair _pair, int *index)
     {
-        if (root != NULL) {
-    
-            // If the node is found
-                if (root->pair->first == _pair.first) {
-    
-                // Replace root with its
-                // left child
+        if (root != NULL) 
+        {
+                if (root->pair->first == _pair.first)
+                {
                 if (root->right == NULL && root->left != NULL)
                 {
                     if (root->par != NULL) 
@@ -451,23 +341,18 @@ class avl {
                             root->par->right = root->left;
                         else
                             root->par->left = root->left;
-    
-                        // Update the height
-                        // of root's parent
                         Updateheight(root->par);
                     }
     
                     root->left->par = root->par;
     
-                    // Balance the node
-                    // after deletion
+
                     root->left = Balance(root->left);
     
                     return root->left;
                 }
     
-                // Replace root with its
-                // right child
+
                 else if (root->left == NULL && root->right != NULL) 
                 {
                     if (root->par != NULL) 
@@ -477,21 +362,16 @@ class avl {
                         else
                             root->par->left = root->right;
     
-                        // Update the height
-                        // of the root's parent
+
                         Updateheight(root->par);
                     }
     
                     root->right->par = root->par;
-    
-                    // Balance the node after
-                    // deletion
+
                     root->right = Balance(root->right);
                     return root->right;
                 }
     
-                // Remove the references of
-                // the current node
                 else if (root->left == NULL && root->right == NULL) 
                 {
                     if ( root->par && _comp(root->par->pair->first, root->pair->first)) {
@@ -508,11 +388,7 @@ class avl {
                     root = NULL;
                     return NULL;
                 }
-    
-                // Otherwise, replace the
-                // current node with its
-                // successor and then
-                // recursively call Delete()
+
                 else 
                 {
                     node_type* tmpnode = root;
@@ -521,50 +397,36 @@ class avl {
                         tmpnode = tmpnode->left;
     
                     pair *val =  tmpnode->pair;
-                    // tmpnode->pair;
     
                     root->right = Delete(root->right, *tmpnode->pair,index);
     
                     root->pair = val;
-    
-                    // Balance the node
-                    // after deletion
+
                     root = Balance(root);
                 }
             }
     
 
-            // Recur to the right subtree to
-            // delete the current node
+
             else if (_comp(root->pair->first, _pair.first))
              {
                 root->right = Delete(root->right, _pair,index);
     
                 root = Balance(root);
             }
-    
-            // Recur into the right subtree
-            // to delete the current node
+
             else if (!_comp(root->pair->first, _pair.first)) {
                 root->left = Delete(root->left, _pair,index);
     
                 root = Balance(root);
             }
     
-            // Update height of the root
-            if (root != NULL) {
+            if (root != NULL) 
                 Updateheight(root);
-            }
         }
-    
-        // Handle the case when the key to be
-        // deleted could not be found
+
         else 
-        {
             *index = 0;
-        }
-    
-        // Return the root node
         return root;
     }
 
